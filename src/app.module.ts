d27d074bdf2f86cf1a09bafbb85ee40cb21619a0
gian <michaelgiantiqui3@gmail.com>
2024-09-16 21:13:31 +0800

    fastlane api

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { UserService } from './service/user.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || '',
+      database: process.env.DB_NAME || 'fastlane',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
---
+++ src/service/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entity/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.userRepo.findOneBy({ id });
+  }
+
+  create(user: User): Promise<User> {
+    return this.userRepo.save(user);
+  }
+
+  update(user: User): Promise<User> {
+    return this.userRepo.save(user);
+  }
+
+  delete(id: number): string {
+    this.userRepo.delete(id);
+
+    return 'user deleted';
+  }
+}
