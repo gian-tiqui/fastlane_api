4269194293d7a22d42433f850a5c95f0ac023993
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-17 15:57:34 +0800

    meow

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { UserModule } from './user/user.module';
     }),
     TypeOrmModule.forFeature([User]),
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
