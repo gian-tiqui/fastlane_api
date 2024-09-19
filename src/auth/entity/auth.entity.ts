394cdcaadb90646631d5372f27108c0ab911e8e8
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-19 11:35:59 +0800

    changed user and refresh token model

--- src/auth/entity/auth.entity.ts
+++ src/auth/entity/auth.entity.ts
@@ -1,15 +1,16 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from '../../user/entity/user.entity';
 
 @Entity()
 export class RefreshToken {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ length: 500 })
   token: string;
 
-  @Column()
-  userId: number;
+  @ManyToOne(() => User, (user) => user.refreshTokens, { onDelete: 'CASCADE' })
+  user: User;
 
   @Column()
   expiresAt: Date;
