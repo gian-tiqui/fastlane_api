a94015bea1342cc65f0ef53a746f4aa388f5b72d
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-20 13:09:32 +0800

    added comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/*
+ * App initializer.
+ */
+
 async function bootstrap() {
   const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
